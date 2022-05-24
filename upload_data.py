bfa7e758d1b36045e05b48c9ce178a83588b2ddf
Natalia Lyzhina <Natalia_Lyzhina@epam.com>
2022-05-24 22:44:08 +0300

    add some minor improvements to upload_data

--- upload_data.py
+++ upload_data.py
@@ -19,10 +19,12 @@ def download_csv(file_url, file_name):
 def upload_to_pg(engine, file_name_athlets, file_name_regions, table_name_w, table_name_s, table_name_r):
 
     df_regions = pd.read_csv(file_name_regions)
+    df_regions = df_regions.loc[:, ['NOC', 'region']]
+
     df_regions.head(n=0).to_sql(name=table_name_r, con=engine,
-                               if_exists='replace', index=False)
+                                if_exists='replace', index=False)
     df_regions.to_sql(name=table_name_r, con=engine,
-                     if_exists='append', index=False)
+                      if_exists='append', index=False)
 
     print("Uploading process finished for regions data.")
 
@@ -83,29 +85,36 @@ def simple_validation(engine, file_name_athlets, file_name_regions, table_name_w
     output_summer_rows = df_s.loc[:, 'count'].item()
     output_regions_rows = df_r.loc[:, 'count'].item()
 
-    print(f"CSV file {file_name_regions} contains {input_regions_rows}.")
+    print(f"CSV file {file_name_regions} contains {input_regions_rows} rows.")
     print(f"Uploaded to postgres {output_regions_rows} rows.")
-    print(f"CSV file {file_name_athlets} contains {input_athlets_rows}.")
+    print(f"CSV file {file_name_athlets} contains {input_athlets_rows} rows.")
     print(
         f"Uploaded to postgres {output_winter_rows} for winter and {output_summer_rows} for summer, at all: {output_winter_rows + output_summer_rows}.")
 
 
 if __name__ == '__main__':
-
+    # url to free .csv dataset with athlets
     FILE_URL_athlets = "https://10d9b011-755b-4b59-9f64-dbfb2ae95d87.filesusr.com/ugd/b84e29_0668e8b9f89b455aafe12f024d788b71.csv?dn=Olympic%20Athletes%20and%20Events.csv"
-    FILE_NAME_athlets = "athlets_and_events.csv"
+    # url to free .csv dataset with lookup table for regions
     FILE_URL_regions = "https://10d9b011-755b-4b59-9f64-dbfb2ae95d87.filesusr.com/ugd/b84e29_aab029d948a64ad3803f945d49071b30.csv?dn=Olympic%20Nations%20and%20Region%20Look-Up.csv"
+
+    # filename to save this file locally
+    FILE_NAME_athlets = "athlets_and_events.csv"
+    # filename to save this file locally
     FILE_NAME_regions = "nations_and_regions.csv"
 
-    USER = 'root'
-    PASSWORD = 'root'
-    HOST = 'pgdatabase'
-    PORT = '5432'
-    DB = 'olympic_database'
+    # credentials for postgres database from container
+    USER = "root"
+    PASSWORD = "root"
+    # parmeters to database connection
+    HOST = "pgdatabase"
+    PORT = "5432"
+    DB = "olympic_database"
 
-    table_name_w = "athlets_and_events_winter"
-    table_name_s = "athlets_and_events_summer"
-    table_name_r = "nations_and_regions"
+    # default tablenames for database
+    TABLE_NAME_WINTER = "athlets_and_events_winter"
+    TABLE_NAME_SUMMER = "athlets_and_events_summer"
+    TABLE_NAME_REGIONS = "nations_and_regions"
 
     try:
         download_csv(FILE_URL_athlets, FILE_NAME_athlets)
@@ -119,9 +128,11 @@ if __name__ == '__main__':
             print(
                 "Cannot connect to postgres. Pls check that docker container has been runned succesfully.")
 
-        upload_to_pg(engine, FILE_NAME_athlets, FILE_NAME_regions, table_name_w, table_name_s, table_name_r)
+        upload_to_pg(engine, FILE_NAME_athlets, FILE_NAME_regions,
+                     TABLE_NAME_WINTER, TABLE_NAME_SUMMER, TABLE_NAME_REGIONS)
 
-        simple_validation(engine, FILE_NAME_athlets, FILE_NAME_regions, table_name_w, table_name_s, table_name_r)
+        simple_validation(engine, FILE_NAME_athlets, FILE_NAME_regions,
+                          TABLE_NAME_WINTER, TABLE_NAME_SUMMER, TABLE_NAME_REGIONS)
     except:
         print("Smth went wrong, sorry:( ")
     else:
